b696f6e1ab98a28aa3f55a76b39879d56475001e
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2018-04-15 18:58:25 +0800

    more license stuffs

--- LibDeflate.lua
+++ LibDeflate.lua
@@ -1,5 +1,20 @@
--- Author: Haoqian He (Github: SafeteeWoW)
--- License: GPLv3
+--[[
+	LibDeflate: Pure Lua implemenation of the DEFLATE lossless data compression algorithm.
+    Copyright (C) <2018>  Haoqian He (Github: SafeteeWoW; World of Warcraft: Safetyy-Illidan(US))
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--]]
 
 require "bit"
 --require "profiler"
