a66f8aca84fde378b90ea43174e606961e0ab3bc
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2018-04-18 03:12:39 +0800

    Update appveyor

--- .appveyor/install-compressor-decompressor.cmd
+++ .appveyor/install-compressor-decompressor.cmd
@@ -2,12 +2,12 @@ set PATH=C:\MinGW\bin;%PATH%
 set ZIP_ZLIB=zlib1211.zip
 
 echo Setting up Puff
-if NOT EXIST "zlib-1.2.11\contrib\puff\puff" (
+if NOT EXIST "zlib-1.2.11\contrib\puff\puff.exe" (
     @echo on
     echo Fetching zlib-1.2.11 From the Internet
     curl -fLsS -o %ZIP_ZLIB% http://www.zlib.net/%ZIP_ZLIB%
-    unzip -f -d . %ZIP_ZLIB%
-    mingw32-gcc zlib-1.2.11\contrib\puff\puff.c zlib-1.2.11\contrib\puff\pufftest.c -o zlib-1.2.11\contrib\puff\puff
+    unzip -o -d . %ZIP_ZLIB%
+    mingw32-gcc zlib-1.2.11\contrib\puff\puff.c zlib-1.2.11\contrib\puff\pufftest.c -o zlib-1.2.11\contrib\puff\puff.exe
 ) else (
     echo Using cached version of puff
 )
