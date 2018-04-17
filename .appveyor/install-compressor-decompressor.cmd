14e0ce62ba6634b28fb65641de2cbaf16820d64c
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2018-04-18 01:37:01 +0800

    Try appveyor

---
+++ .appveyor/install-compressor-decompressor.cmd
@@ -0,0 +1,15 @@
+set ZIP_ZLIB=zlib1211.zip
+
+echo Setting up Puff
+if NOT EXIST "zlib-1.2.11\contrib\puff\puff" (
+    @echo on
+    echo Fetching zlib-1.2.11 From the Internet
+    curl -fLsS -o %ZIP_ZLIB% http://www.zlib.net/%ZIP_ZLIB%
+    unzip -d . %ZIP_ZLIB%
+    mingw32-gcc zlib-1.2.11\contrib\puff\puff.c zlib-1.2.11\contrib\puff\pufftest.c -o zlib-1.2.11\contrib\puff\puff
+) else (
+    echo Using cached version of puff
+)
+set PATH=%APPVEYOR_BUILD_FOLDER%\zlib-1.2.11\contrib\puff;%PATH%
+@echo off
+REM goto :EOF
