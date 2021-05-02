e7867bb4848d617a2bbb8fd19b06317d41420322
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2021-05-02 22:16:54 +0800

    [dev] Format C code using clang-format 10.0.0

--- tests/pufftest.c
+++ tests/pufftest.c
@@ -18,14 +18,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "puff.h"
 
-#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__) || defined(_WIN32)
-#  include <fcntl.h>
-#  include <io.h>
-#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__) || \
+    defined(_WIN32)
+#include <fcntl.h>
+#include <io.h>
+#define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
 #else
-#  define SET_BINARY_MODE(file)
+#define SET_BINARY_MODE(file)
 #endif
 
 #define local static
@@ -34,21 +36,18 @@
    3, or 5 times a power of 2 -- the result is always > size, until the result
    is the maximum value of an unsigned long, where it remains.  This is useful
    to keep reallocations less than ~33% over the actual data. */
-local size_t bythirds(size_t size)
-{
-    int n;
-    size_t m;
+local size_t bythirds(size_t size) {
+  int n;
+  size_t m;
 
-    m = size;
-    for (n = 0; m; n++)
-        m >>= 1;
-    if (n < 3)
-        return size + 1;
-    n -= 3;
-    m = size >> n;
-    m += m == 6 ? 2 : 1;
-    m <<= n;
-    return m > size ? m : (size_t)(-1);
+  m = size;
+  for (n = 0; m; n++) m >>= 1;
+  if (n < 3) return size + 1;
+  n -= 3;
+  m = size >> n;
+  m += m == 6 ? 2 : 1;
+  m <<= n;
+  return m > size ? m : (size_t)(-1);
 }
 
 /* Read the input file *name, or stdin if name is NULL, into allocated memory.
@@ -57,114 +56,108 @@ local size_t bythirds(size_t size)
    failure.  *len is the number of bytes of data read from the input file (even
    if load() returns NULL).  If the input file was empty or could not be opened
    or read, *len is zero. */
-local void *load(const char *name, size_t *len)
-{
-    size_t size;
-    void *buf, *swap;
-    FILE *in;
+local void *load(const char *name, size_t *len) {
+  size_t size;
+  void *buf, *swap;
+  FILE *in;
 
-    *len = 0;
-    buf = malloc(size = 4096);
-    if (buf == NULL)
-        return NULL;
-    in = name == NULL ? stdin : fopen(name, "rb");
-    if (in != NULL) {
-        for (;;) {
-            *len += fread((char *)buf + *len, 1, size - *len, in);
-            if (*len < size) break;
-            size = bythirds(size);
-            if (size == *len || (swap = realloc(buf, size)) == NULL) {
-                free(buf);
-                buf = NULL;
-                break;
-            }
-            buf = swap;
-        }
-        fclose(in);
+  *len = 0;
+  buf = malloc(size = 4096);
+  if (buf == NULL) return NULL;
+  in = name == NULL ? stdin : fopen(name, "rb");
+  if (in != NULL) {
+    for (;;) {
+      *len += fread((char *)buf + *len, 1, size - *len, in);
+      if (*len < size) break;
+      size = bythirds(size);
+      if (size == *len || (swap = realloc(buf, size)) == NULL) {
+        free(buf);
+        buf = NULL;
+        break;
+      }
+      buf = swap;
     }
-    return buf;
+    fclose(in);
+  }
+  return buf;
 }
 
-int main(int argc, char **argv)
-{
-    SET_BINARY_MODE(stdin);
-    SET_BINARY_MODE(stdout);
-    SET_BINARY_MODE(stderr);
+int main(int argc, char **argv) {
+  SET_BINARY_MODE(stdin);
+  SET_BINARY_MODE(stdout);
+  SET_BINARY_MODE(stderr);
 
-    int ret, put = 0, fail = 0;
-    unsigned skip = 0;
-    char *arg, *name = NULL;
-    unsigned char *source = NULL, *dest;
-    size_t len = 0;
-    unsigned long sourcelen, destlen;
+  int ret, put = 0, fail = 0;
+  unsigned skip = 0;
+  char *arg, *name = NULL;
+  unsigned char *source = NULL, *dest;
+  size_t len = 0;
+  unsigned long sourcelen, destlen;
 
-    /* process arguments */
-    while (arg = *++argv, --argc)
-        if (arg[0] == '-') {
-            if (arg[1] == 'w' && arg[2] == 0)
-                put = 1;
-            else if (arg[1] == 'f' && arg[2] == 0)
-                fail = 1, put = 1;
-            else if (arg[1] >= '0' && arg[1] <= '9')
-                skip = (unsigned)atoi(arg + 1);
-            else {
-                fprintf(stderr, "invalid option %s\n", arg);
-                return 3;
-            }
-        }
-        else if (name != NULL) {
-            fprintf(stderr, "only one file name allowed\n");
-            return 3;
-        }
-        else
-            name = arg;
-    source = load(name, &len);
-    if (source == NULL) {
-        fprintf(stderr, "memory allocation failure\n");
-        return 4;
-    }
-    if (len == 0) {
-        fprintf(stderr, "could not read %s, or it was empty\n",
-                name == NULL ? "<stdin>" : name);
-        free(source);
+  /* process arguments */
+  while (arg = *++argv, --argc)
+    if (arg[0] == '-') {
+      if (arg[1] == 'w' && arg[2] == 0)
+        put = 1;
+      else if (arg[1] == 'f' && arg[2] == 0)
+        fail = 1, put = 1;
+      else if (arg[1] >= '0' && arg[1] <= '9')
+        skip = (unsigned)atoi(arg + 1);
+      else {
+        fprintf(stderr, "invalid option %s\n", arg);
         return 3;
-    }
-    if (skip >= len) {
-        fprintf(stderr, "skip request of %d leaves no input\n", skip);
-        free(source);
-        return 3;
-    }
-
-    /* test inflate data with offset skip */
-    len -= skip;
-    sourcelen = (unsigned long)len;
-    ret = puff(NIL, &destlen, source + skip, &sourcelen);
-    if (ret)
-        fprintf(stderr, "puff() failed with return code %d\n", ret);
-    else {
-        if (sourcelen < len) {
-            // LibDeflate: output unprocessed bytes
-            fprintf(stderr, "%lu", len - sourcelen);
-        }
-        //fprintf(stderr, "puff() succeeded uncompressing %lu bytes\n", destlen);
-    }
-
-    /* if requested, inflate again and write decompressd data to stdout */
-    if (put && ret == 0) {
-        if (fail)
-            destlen >>= 1;
-        dest = malloc(destlen);
-        if (dest == NULL) {
-            fprintf(stderr, "memory allocation failure\n");
-            free(source);
-            return 4;
-        }
-        puff(dest, &destlen, source + skip, &sourcelen);
-        fwrite(dest, 1, destlen, stdout);
-        free(dest);
-    }
-
-    /* clean up */
+      }
+    } else if (name != NULL) {
+      fprintf(stderr, "only one file name allowed\n");
+      return 3;
+    } else
+      name = arg;
+  source = load(name, &len);
+  if (source == NULL) {
+    fprintf(stderr, "memory allocation failure\n");
+    return 4;
+  }
+  if (len == 0) {
+    fprintf(stderr, "could not read %s, or it was empty\n",
+            name == NULL ? "<stdin>" : name);
     free(source);
-    return ret;
+    return 3;
+  }
+  if (skip >= len) {
+    fprintf(stderr, "skip request of %d leaves no input\n", skip);
+    free(source);
+    return 3;
+  }
+
+  /* test inflate data with offset skip */
+  len -= skip;
+  sourcelen = (unsigned long)len;
+  ret = puff(NIL, &destlen, source + skip, &sourcelen);
+  if (ret)
+    fprintf(stderr, "puff() failed with return code %d\n", ret);
+  else {
+    if (sourcelen < len) {
+      // LibDeflate: output unprocessed bytes
+      fprintf(stderr, "%lu", len - sourcelen);
+    }
+    // fprintf(stderr, "puff() succeeded uncompressing %lu bytes\n", destlen);
+  }
+
+  /* if requested, inflate again and write decompressd data to stdout */
+  if (put && ret == 0) {
+    if (fail) destlen >>= 1;
+    dest = malloc(destlen);
+    if (dest == NULL) {
+      fprintf(stderr, "memory allocation failure\n");
+      free(source);
+      return 4;
+    }
+    puff(dest, &destlen, source + skip, &sourcelen);
+    fwrite(dest, 1, destlen, stdout);
+    free(dest);
+  }
+
+  /* clean up */
+  free(source);
+  return ret;
 }
--- tests/zdeflate.c
+++ tests/zdeflate.c
@@ -1,6 +1,6 @@
 /*
 This is a C program supports Deflate/Zlib compression and decompression.
-This code is modified from zpipe.c from the source code repository of 
+This code is modified from zpipe.c from the source code repository of
 the Zlib project. This program is used to test the correctness of LibDeflate.
 */
 
@@ -18,18 +18,20 @@ the Zlib project. This program is used to test the correctness of LibDeflate.
                      Avoid some compiler warnings for input and output buffers
  */
 
-#include <stdio.h>
-#include <string.h>
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "zlib.h"
 
-#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__) || defined(_WIN32)
-#  include <fcntl.h>
-#  include <io.h>
-#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__) || \
+    defined(_WIN32)
+#include <fcntl.h>
+#include <io.h>
+#define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
 #else
-#  define SET_BINARY_MODE(file)
+#define SET_BINARY_MODE(file)
 #endif
 
 #define CHUNK 16384
@@ -41,56 +43,54 @@ the Zlib project. This program is used to test the correctness of LibDeflate.
    level is supplied, Z_VERSION_ERROR if the version of zlib.h and the
    version of the library linked do not match, or Z_ERRNO if there is
    an error reading or writing the files. */
-int def(FILE *source, FILE *dest, int level, int strategy, int isZlib, char* dictionary, int dictSize)
-{
-    int ret, flush;
-    unsigned have;
-    z_stream strm;
-    unsigned char in[CHUNK];
-    unsigned char out[CHUNK];
+int def(FILE* source, FILE* dest, int level, int strategy, int isZlib,
+        char* dictionary, int dictSize) {
+  int ret, flush;
+  unsigned have;
+  z_stream strm;
+  unsigned char in[CHUNK];
+  unsigned char out[CHUNK];
 
-    /* allocate deflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    ret = deflateInit2(&strm, level, Z_DEFLATED, isZlib?15:-15, 8, strategy);
-    if (ret != Z_OK)
-        return ret;
-    if (dictionary)
-        deflateSetDictionary(&strm, dictionary, dictSize);
+  /* allocate deflate state */
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+  ret = deflateInit2(&strm, level, Z_DEFLATED, isZlib ? 15 : -15, 8, strategy);
+  if (ret != Z_OK) return ret;
+  if (dictionary) deflateSetDictionary(&strm, dictionary, dictSize);
 
-    /* compress until end of file */
+  /* compress until end of file */
+  do {
+    strm.avail_in = fread(in, 1, CHUNK, source);
+    if (ferror(source)) {
+      (void)deflateEnd(&strm);
+      return Z_ERRNO;
+    }
+    flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
+    strm.next_in = in;
+
+    /* run deflate() on input until output buffer not full, finish
+       compression if all of source has been read in */
     do {
-        strm.avail_in = fread(in, 1, CHUNK, source);
-        if (ferror(source)) {
-            (void)deflateEnd(&strm);
-            return Z_ERRNO;
-        }
-        flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
-        strm.next_in = in;
+      strm.avail_out = CHUNK;
+      strm.next_out = out;
+      ret = deflate(&strm, flush);   /* no bad return value */
+      assert(ret != Z_STREAM_ERROR); /* state not clobbered */
+      have = CHUNK - strm.avail_out;
+      if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+        (void)deflateEnd(&strm);
+        return Z_ERRNO;
+      }
+    } while (strm.avail_out == 0);
+    assert(strm.avail_in == 0); /* all input will be used */
 
-        /* run deflate() on input until output buffer not full, finish
-           compression if all of source has been read in */
-        do {
-            strm.avail_out = CHUNK;
-            strm.next_out = out;
-            ret = deflate(&strm, flush);    /* no bad return value */
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-            have = CHUNK - strm.avail_out;
-            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-                (void)deflateEnd(&strm);
-                return Z_ERRNO;
-            }
-        } while (strm.avail_out == 0);
-        assert(strm.avail_in == 0);     /* all input will be used */
+    /* done when last data in file processed */
+  } while (flush != Z_FINISH);
+  assert(ret == Z_STREAM_END); /* stream will be complete */
 
-        /* done when last data in file processed */
-    } while (flush != Z_FINISH);
-    assert(ret == Z_STREAM_END);        /* stream will be complete */
-
-    /* clean up and return */
-    (void)deflateEnd(&strm);
-    return Z_OK;
+  /* clean up and return */
+  (void)deflateEnd(&strm);
+  return Z_OK;
 }
 
 /* Decompress from file source to file dest until stream ends or EOF.
@@ -99,208 +99,198 @@ int def(FILE *source, FILE *dest, int level, int strategy, int isZlib, char* dic
    invalid or incomplete, Z_VERSION_ERROR if the version of zlib.h and
    the version of the library linked do not match, or Z_ERRNO if there
    is an error reading or writing the files. */
-int inf(FILE *source, FILE *dest, int isZlib, char* dictionary, int dictSize)
-{
-    int ret;
-    unsigned have;
-    z_stream strm;
-    unsigned char in[CHUNK];
-    unsigned char out[CHUNK];
+int inf(FILE* source, FILE* dest, int isZlib, char* dictionary, int dictSize) {
+  int ret;
+  unsigned have;
+  z_stream strm;
+  unsigned char in[CHUNK];
+  unsigned char out[CHUNK];
 
-    /* allocate inflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.avail_in = 0;
-    strm.next_in = Z_NULL;
-    ret = inflateInit2(&strm, isZlib?15:-15);
-    if (ret != Z_OK)
-        return ret;
-    if (dictionary && !isZlib)
-        inflateSetDictionary(&strm, dictionary, dictSize);
+  /* allocate inflate state */
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+  strm.avail_in = 0;
+  strm.next_in = Z_NULL;
+  ret = inflateInit2(&strm, isZlib ? 15 : -15);
+  if (ret != Z_OK) return ret;
+  if (dictionary && !isZlib) inflateSetDictionary(&strm, dictionary, dictSize);
 
-    /* decompress until deflate stream ends or end of file */
-    do {
-        strm.avail_in = fread(in, 1, CHUNK, source);
-        if (ferror(source)) {
-            (void)inflateEnd(&strm);
-            return Z_ERRNO;
-        }
-        if (strm.avail_in == 0)
-            break;
-        strm.next_in = in;
-
-        int set_dictionary_zlib = 0;
-        /* run inflate() on input until output buffer not full */
-        do {
-            set_dictionary_zlib = 0 ;
-            strm.avail_out = CHUNK;
-            strm.next_out = out;
-            ret = inflate(&strm, Z_NO_FLUSH);
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-            switch (ret) {
-            case Z_NEED_DICT:
-                if (!dictionary)
-                    ret = Z_DATA_ERROR;     /* and fall through */
-                else {
-                    ret = inflateSetDictionary(&strm, dictionary, dictSize);
-                    if (ret == Z_OK) {
-                        set_dictionary_zlib = 1;
-                        break;
-                    } // LibDeflate: Bad problem practice
-                }
-            case Z_DATA_ERROR:
-            case Z_MEM_ERROR:
-                (void)inflateEnd(&strm);
-                return ret;
-            }
-            if (!set_dictionary_zlib) {
-                have = CHUNK - strm.avail_out;
-                if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-                    (void)inflateEnd(&strm);
-                    return Z_ERRNO;
-                }
-            }
-        } while (strm.avail_out == 0 || set_dictionary_zlib);
-
-        /* done when inflate() says it's done */
-    } while (ret != Z_STREAM_END);
-
-    /* clean up and return */
-    (void)inflateEnd(&strm);
-    if (ret == Z_STREAM_END) {
-        int fPos = ftell(source);
-        fseek(source, 0, SEEK_END);
-        int unprocessed = ftell(source)-fPos+strm.avail_in;
-        if (unprocessed > 0) {
-            fprintf(stderr, "%d", unprocessed);
-        }
-        return Z_OK;
+  /* decompress until deflate stream ends or end of file */
+  do {
+    strm.avail_in = fread(in, 1, CHUNK, source);
+    if (ferror(source)) {
+      (void)inflateEnd(&strm);
+      return Z_ERRNO;
     }
-    else
-        return Z_DATA_ERROR;
+    if (strm.avail_in == 0) break;
+    strm.next_in = in;
+
+    int set_dictionary_zlib = 0;
+    /* run inflate() on input until output buffer not full */
+    do {
+      set_dictionary_zlib = 0;
+      strm.avail_out = CHUNK;
+      strm.next_out = out;
+      ret = inflate(&strm, Z_NO_FLUSH);
+      assert(ret != Z_STREAM_ERROR); /* state not clobbered */
+      switch (ret) {
+        case Z_NEED_DICT:
+          if (!dictionary)
+            ret = Z_DATA_ERROR; /* and fall through */
+          else {
+            ret = inflateSetDictionary(&strm, dictionary, dictSize);
+            if (ret == Z_OK) {
+              set_dictionary_zlib = 1;
+              break;
+            }  // LibDeflate: Bad problem practice
+          }
+        case Z_DATA_ERROR:
+        case Z_MEM_ERROR:
+          (void)inflateEnd(&strm);
+          return ret;
+      }
+      if (!set_dictionary_zlib) {
+        have = CHUNK - strm.avail_out;
+        if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+          (void)inflateEnd(&strm);
+          return Z_ERRNO;
+        }
+      }
+    } while (strm.avail_out == 0 || set_dictionary_zlib);
+
+    /* done when inflate() says it's done */
+  } while (ret != Z_STREAM_END);
+
+  /* clean up and return */
+  (void)inflateEnd(&strm);
+  if (ret == Z_STREAM_END) {
+    int fPos = ftell(source);
+    fseek(source, 0, SEEK_END);
+    int unprocessed = ftell(source) - fPos + strm.avail_in;
+    if (unprocessed > 0) {
+      fprintf(stderr, "%d", unprocessed);
+    }
+    return Z_OK;
+  } else
+    return Z_DATA_ERROR;
 }
 
 /* report a zlib or i/o error */
-void zerr(int ret)
-{
-    fputs("zpipe: ", stderr);
-    switch (ret) {
+void zerr(int ret) {
+  fputs("zpipe: ", stderr);
+  switch (ret) {
     case Z_ERRNO:
-        if (ferror(stdin))
-            fputs("error reading stdin\n", stderr);
-        if (ferror(stdout))
-            fputs("error writing stdout\n", stderr);
-        break;
+      if (ferror(stdin)) fputs("error reading stdin\n", stderr);
+      if (ferror(stdout)) fputs("error writing stdout\n", stderr);
+      break;
     case Z_STREAM_ERROR:
-        fputs("invalid compression level\n", stderr);
-        break;
+      fputs("invalid compression level\n", stderr);
+      break;
     case Z_DATA_ERROR:
-        fputs("invalid or incomplete deflate data\n", stderr);
-        break;
+      fputs("invalid or incomplete deflate data\n", stderr);
+      break;
     case Z_MEM_ERROR:
-        fputs("out of memory\n", stderr);
-        break;
+      fputs("out of memory\n", stderr);
+      break;
     case Z_VERSION_ERROR:
-        fputs("zlib version mismatch!\n", stderr);
+      fputs("zlib version mismatch!\n", stderr);
     case EXTRA_BYTE_AFTER_STREAM_ERROR:
-        fputs("Extra bytes after deflate data\n", stderr);
-    }
+      fputs("Extra bytes after deflate data\n", stderr);
+  }
 }
 
 /* compress or decompress from stdin to stdout */
-int main(int argc, char **argv)
-{
-    int ret;
+int main(int argc, char** argv) {
+  int ret;
 
-    /* avoid end-of-line conversions */
-    SET_BINARY_MODE(stdin);
-    SET_BINARY_MODE(stdout);
-    SET_BINARY_MODE(stderr);
+  /* avoid end-of-line conversions */
+  SET_BINARY_MODE(stdin);
+  SET_BINARY_MODE(stdout);
+  SET_BINARY_MODE(stderr);
 
-    int level = Z_DEFAULT_COMPRESSION;
-    int strategy = Z_DEFAULT_STRATEGY;
-    int isDecompress = 0;
-    int isZlib = 0;
-    char* dictionary = 0;
-    int dictSize = 0;
+  int level = Z_DEFAULT_COMPRESSION;
+  int strategy = Z_DEFAULT_STRATEGY;
+  int isDecompress = 0;
+  int isZlib = 0;
+  char* dictionary = 0;
+  int dictSize = 0;
 
-    int i = 0;
-    for (i = 1; i < argc; ++i) {
-        char* arg = argv[i];
-        if (strcmp(arg, "-d") == 0)
-            isDecompress = 1;
-        else if (strcmp(arg, "--zlib") == 0)
-            isZlib = 1;
-        else if (strcmp(arg, "-0") == 0)
-            level = 0;
-        else if (strcmp(arg, "-1") == 0)
-            level = 1;
-        else if (strcmp(arg, "-2") == 0)
-            level = 2;
-        else if (strcmp(arg, "-3") == 0)
-            level = 3;
-        else if (strcmp(arg, "-4") == 0)
-            level = 4;
-        else if (strcmp(arg, "-5") == 0)
-            level = 5;
-        else if (strcmp(arg, "-6") == 0)
-            level = 6;
-        else if (strcmp(arg, "-7") == 0)
-            level = 7;
-        else if (strcmp(arg, "-8") == 0)
-            level = 8;
-        else if (strcmp(arg, "-9") == 0)
-            level = 9;
-        else if (strcmp(arg, "--filter") == 0)
-            strategy = Z_FILTERED;
-        else if (strcmp(arg, "--huffman") == 0)
-            strategy = Z_HUFFMAN_ONLY;
-        else if (strcmp(arg, "--rle") == 0)
-            strategy = Z_RLE;
-        else if (strcmp(arg, "--fix") == 0)
-            strategy = Z_FIXED;
-        else if (strcmp(arg, "--default") == 0)
-            strategy = Z_DEFAULT_STRATEGY;
-        else if (strcmp(arg, "--dict") == 0) {
-            dictionary = (char*)malloc(32769);
-            i++;
-            char* filename = argv[i];
-            FILE* file = fopen(filename, "rb");
-            if (file) {
-                fseek(file, 0, SEEK_END);
-                dictSize = ftell(file);
-                rewind(file);
-                fread(dictionary, 1, dictSize, file);
-                dictionary[dictSize] = 0;
-                fclose(file);
-            } else {
-                fprintf(stderr, "Cant open dictionary file %s", filename);
-                return 255;
-            }
-        }
-        else {
-            fputs("zpipe usage: zpipe [-d] [--zlib] [-0/-1/.../-9] "
-                "[--filter/--huffman/--rle/--fix/--default] "
-                "< source > dest\n", stderr);
-        return 1;
-        }
-    }
-    /* do compression if no arguments */
-    if (!isDecompress) {
-        ret = def(stdin, stdout, level, strategy, isZlib, dictionary, dictSize);
-        if (ret != Z_OK)
-            zerr(ret);
-        return ret;
+  int i = 0;
+  for (i = 1; i < argc; ++i) {
+    char* arg = argv[i];
+    if (strcmp(arg, "-d") == 0)
+      isDecompress = 1;
+    else if (strcmp(arg, "--zlib") == 0)
+      isZlib = 1;
+    else if (strcmp(arg, "-0") == 0)
+      level = 0;
+    else if (strcmp(arg, "-1") == 0)
+      level = 1;
+    else if (strcmp(arg, "-2") == 0)
+      level = 2;
+    else if (strcmp(arg, "-3") == 0)
+      level = 3;
+    else if (strcmp(arg, "-4") == 0)
+      level = 4;
+    else if (strcmp(arg, "-5") == 0)
+      level = 5;
+    else if (strcmp(arg, "-6") == 0)
+      level = 6;
+    else if (strcmp(arg, "-7") == 0)
+      level = 7;
+    else if (strcmp(arg, "-8") == 0)
+      level = 8;
+    else if (strcmp(arg, "-9") == 0)
+      level = 9;
+    else if (strcmp(arg, "--filter") == 0)
+      strategy = Z_FILTERED;
+    else if (strcmp(arg, "--huffman") == 0)
+      strategy = Z_HUFFMAN_ONLY;
+    else if (strcmp(arg, "--rle") == 0)
+      strategy = Z_RLE;
+    else if (strcmp(arg, "--fix") == 0)
+      strategy = Z_FIXED;
+    else if (strcmp(arg, "--default") == 0)
+      strategy = Z_DEFAULT_STRATEGY;
+    else if (strcmp(arg, "--dict") == 0) {
+      dictionary = (char*)malloc(32769);
+      i++;
+      char* filename = argv[i];
+      FILE* file = fopen(filename, "rb");
+      if (file) {
+        fseek(file, 0, SEEK_END);
+        dictSize = ftell(file);
+        rewind(file);
+        fread(dictionary, 1, dictSize, file);
+        dictionary[dictSize] = 0;
+        fclose(file);
+      } else {
+        fprintf(stderr, "Cant open dictionary file %s", filename);
+        return 255;
+      }
     } else {
-        ret = inf(stdin, stdout, isZlib, dictionary, dictSize);
-        if (ret != Z_OK)
-            zerr(ret);
-        return ret;
+      fputs(
+          "zpipe usage: zpipe [-d] [--zlib] [-0/-1/.../-9] "
+          "[--filter/--huffman/--rle/--fix/--default] "
+          "< source > dest\n",
+          stderr);
+      return 1;
     }
+  }
+  /* do compression if no arguments */
+  if (!isDecompress) {
+    ret = def(stdin, stdout, level, strategy, isZlib, dictionary, dictSize);
+    if (ret != Z_OK) zerr(ret);
+    return ret;
+  } else {
+    ret = inf(stdin, stdout, isZlib, dictionary, dictSize);
+    if (ret != Z_OK) zerr(ret);
+    return ret;
+  }
 
-    if (dictionary) {
-        free(dictionary);
-    }
-    return 0;
+  if (dictionary) {
+    free(dictionary);
+  }
+  return 0;
 }
