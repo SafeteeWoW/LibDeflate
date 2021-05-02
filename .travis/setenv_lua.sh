3370280628711774dde3dc56dbc52aa0160fa3d1
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2021-05-02 20:21:33 +0800

    [ci] Remove appveyor and travis

--- .travis/setenv_lua.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p $HOME/install
-export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${HOME}/install/luarocks/bin
-bash .travis/setup_lua.sh
-eval `$HOME/.lua/luarocks path`
-cd ${TRAVIS_BUILD_DIR}
--- .travis/setup_compressor_decompressor.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ${HOME}/install
-cd ${HOME}/install && curl --retry 10 --retry-delay 10 --location http://www.zlib.net/zlib-1.2.11.tar.gz | tar xz
-cd ${HOME}/install/zlib-1.2.11/ && ./configure && make -j4
-export ZLIB_PATH=${HOME}/install/zlib-1.2.11
-cd ${TRAVIS_BUILD_DIR}/tests
-make -f Makefile_Unix
-export PATH=:${TRAVIS_BUILD_DIR}/tests:${PATH}
-cd ${TRAVIS_BUILD_DIR}
