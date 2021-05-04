405657d245026e7540e089cbfd42a72e22da96df
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2021-05-05 00:07:09 +0800

    [dev] Update python test script

--- tests/dev_scripts/test_from_random_files_in_disk.py
+++ tests/dev_scripts/test_from_random_files_in_disk.py
@@ -1,9 +1,16 @@
-# 1st argument: a directory
-# Call this script in the folder that contains LibDeflate.lua
-# Record all small files in the directory and its subdir in a list
-# and continues to randomly pick up a file from the list.
-# And run lua test file on the file.
-# If fails, abort and show the file name
+#!/usr/bin/env python3
+"""
+Test using random files in disk.
+
+1st argument: a directory
+
+Call this script in the folder that contains LibDeflate.lua
+
+Record all small files in the directory and its subdir in a list
+and continues to randomly pick up a file from the list.
+And run lua test file on the file.
+If fails, abort and show the file name
+"""
 
 import os
 import sys
@@ -17,32 +24,38 @@ def traverse(rootDir):
         for file in files:
             try:
                 if file.find("compress") == -1 \
-                    and file.find(".lnk") == -1 and os.path.getsize(os.path.join(root, file)) <= 1024*1024:
+                    and file.find(".lnk") == -1 \
+                    and os.path.getsize(os.path.join(root, file)) <= 1024*1024:
                     fileList.append(os.path.join(root, file))
-            except WindowsError:
+            except OSError:
                 pass  # Wierd pass
         for dir in dirs:
             traverse(dir)
 
 
-if __name__ == '__main__':
+def main():
     traverse(sys.argv[1])
+    random.shuffle(fileList)
     print("File list has been generated. Start testing")
-    while True:
-        rand = random.randint(0, len(fileList) - 1)
-        file = fileList[rand]
+    for file in fileList:
         try:
             f = open(file, "rb")
             f.close()
-            ret = os.system("luajit tests\\Test.lua -o " + file)
+            ret = os.system("luajit tests/Test.lua -o " + file)
             if ret == 0:  # Lua can open the file.
-                ret = os.system("luajit tests\\Test.lua -c " + file +
-                                " tests\\tmp.compressed")
+                print("Testing file " + file)
+                ret = os.system("luajit tests/Test.lua -c " + file +
+                                " tests/tmp.compressed")
                 if ret == 0:
                     print(file, "OK")
                 else:
                     print(file,
-                          "ERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRROR")
-                    exit(1)
+                          "ERROR: Exit code is " + str(ret),
+                          file=sys.stderr)
+                    exit(ret)
         except Exception as e:
-            print("Python cannot open:", file)
+            print("WARNING: Python cannot open:", file, file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
